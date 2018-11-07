3e2f0fa53ba75d311b71de41a8dd8a5f1dcd5c54
Jesse Groot <31734273+jessegroot@users.noreply.github.com>
2018-11-07 19:47:07 +0100

    Add files via upload

--- IMDBEnd.py
+++ IMDBEnd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-# Name:
-# Student number:
+# Name: Jesse Groot
+# Student number: 11012579
 """
 This script scrapes IMDB and outputs a CSV file with highest rated movies.
 """
@@ -12,7 +12,6 @@ from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-import re
 
 TARGET_URL = "https://www.imdb.com/search/title?title_type=feature&release_dat\
 e=2008-01-01,2018-01-01&num_votes=5000,&sort=user_rating,desc"
@@ -21,66 +20,39 @@ OUTPUT_CSV = 'movies.csv'
 
 def extract_movies(dom):
 
-    #storage room for all data
+    # storage room for all data
     allData = []
-    #For loop that goes through all the movies on site.
+    # for loop that goes through all the movies on site
     for films in dom.find_all('div', class_='lister-item-content'):
 
-        #Add title of movie
-        allData.append(str(films.find('a').get_text()))
+        # add title of movie
+        allData.append(films.find('a').get_text())
 
-
-        #Add Ratings of movies
+        # add Ratings of movies
         allData.append(films.find('strong').get_text())
 
-        #Add release_date of the movie minus other signs
+        # add release_date of the movie minus other signs
         release = films.find('span', class_='lister-item-year text-muted unbold').get_text()
         release = int(''.join(filter(str.isdigit, release)))
         allData.append(release)
 
-        #Select the p where the actors are in.
-        # actors = films.find_all('p')[2].get_text()
-
+        # get href part what displays actors in html
         actors = films.select("a[href*='?ref_=adv_li_st']")
-        allActors = []
+        allActors = 0
+        # loop over the ammount of actors
         for actor in actors:
-            if allActors == []:
-                allActors.append(actor.get_text())
+            if allActors == 0:
+                allActors = actor.get_text()
             else:
+                # join actors to one string
                 allActors = ", ".join([allActors, actor.get_text()])
-        # allActors = ", ".join(str(allActors) for x in allActors)
 
         allData.append(allActors)
 
-        # print(actors.get_text())
-        #
-        # #Select strings as wholes.
-        # actors = re.sub("[^\w]", " ", actors).split()
-        # acces = 0
-        # listActors = 0
-        #
-        # #Use forloop to go through all words and select only the titles\
-        # #Who come after Stars.
-        # for actor in actors:
-        #     if acces == 1 or acces == 2:
-        #         if acces == 1:
-        #             firstname = actor
-        #             acces = 2
-        #         else:
-        #             firstname = (''.join([firstname, " ", actor]))
-        #             if listActors == 0:
-        #                 listActors = firstname
-        #             else:
-        #                 listActors = (''.join([listActors, ", ", firstname]))
-        #             acces = 1
-        #     if actor == "Stars":
-        #         acces = 1
-        # allData.append(listActors)
-
-        #Add Genre of the movie minus /n
+        # add Genre of the movie minus /n
         allData.append(films.find('span', class_='genre').get_text()[1:])
 
-        #Add runtime of movie (minus the min ([:-4]))
+        # add runtime of movie (minus the min ([:-4]))
         allData.append(films.find('span', class_='runtime').get_text()[:-4])
 
     """
@@ -92,8 +64,7 @@ def extract_movies(dom):
     - Actors/actresses (comma separated  if more than one)
     - Runtime (only a number!)
     """
-    print(allData)
-    #Return the Movie data
+    # return the movie data
     return [allData]
 
 
@@ -103,10 +74,10 @@ def save_csv(outfile, movies):
     """
     writer = csv.writer(outfile, delimiter=',')
     writer.writerow(['Title', 'Rating', 'Year', 'Actors', 'Genre', 'Runtime'])
-    #loop over the movies and writhing their 6 atributes
+
+    # loop over the movies and writhing their 6 atributes
     for row in range(0,50):
         writer.writerow([movies[0][0+(row*6)], movies[0][1+(row*6)], movies[0][2+(row*6)], movies[0][3+(row*6)], movies[0][4+(row*6)], movies[0][5+(row*6)]])
-
 
 def simple_get(url):
     """
--- Visualizer.py
+++ Visualizer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-# Name:
-# Student number:
+# Name: Jesse Groot
+# Student number: 11012579
 """
 This script visualizes data obtained from a .csv file
 """
@@ -9,24 +9,21 @@ import csv
 import matplotlib.pyplot as plt
 
 
-# Global constants for the input file, first and last year
+# global constants for the input file, first and last year
 INPUT_CSV = "movies.csv"
 START_YEAR = 2008
 END_YEAR = 2018
 
-# Global dictionary for the data
-data_dict = {str(key): [] for key in range(START_YEAR, END_YEAR)}
-
 def getGraph(ratings):
     plotValuesY = []
     plotValuesX = []
-    #append the mean ratings to the plotValuesY en the years to the plotValuesX
+    # append the mean ratings to the plotValuesY en the years to the plotValuesX
     for rating in ratings:
         plotValuesY.append(rating[1]/rating[0])
     for years in range(START_YEAR,END_YEAR):
         plotValuesX.append(years)
 
-    #plot the graph
+    # plot the graph
     plt.plot(plotValuesX,plotValuesY)
     plt.ylabel('Rating Score')
     plt.xlabel('Year of Release')
@@ -39,22 +36,20 @@ if __name__ == "__main__":
         #make reader of the CSV file
         lineReader = csv.DictReader(csvfile)
         couples = []
-        #loop over the lines in CSV file
+        # loop over the lines in CSV file
         for row in lineReader:
-            #make couples per film of rating and year
+            # make couples per film of rating and year
             couples.append([row['Rating'],row['Year']])
 
         meanYear = []
-        #loop over the years
+        # loop over the years
         for years in range(START_YEAR,END_YEAR):
             meanYear.append([0, 0])
-            #sum the ratings from each year
+            # sum the ratings from each year
             for couple in couples:
                 if int(couple[1]) == years:
                     meanYear[years-START_YEAR][1] = meanYear[years-START_YEAR][1] + float(couple[0])
                     meanYear[years-START_YEAR][0] = meanYear[years-START_YEAR][0] + 1
 
-        #give the summed ratings to the graph maker
+        # give the summed ratings to the graph maker
         graph = getGraph(meanYear)
-
-    print(data_dict)
