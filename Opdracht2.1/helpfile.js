9c2f44f50270ed3dddb46f8d31a500523c63c8e4
Jesse Groot <jesse.groot@live.nl>
2018-11-22 12:49:52 +0100

    week_3 canges

---
+++ Opdracht2.1/helpfile.js
@@ -0,0 +1,24 @@
+
+// github.com/airbnb/Javascript
+// camelCasing
+// Semicolons
+//
+/*
+hello world
+*/
+
+var arr = [1,2,3];
+var result = 0;
+arr.forEach(function(d){
+  result += d;
+});
+console.log(result);
+
+// 1 + "2" + 3 = 123 in Javascript
+// typeof()
+
+// == betekend het zelfde vs === is het zelfde.
+
+import requests
+
+// var & char
